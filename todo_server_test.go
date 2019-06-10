ab93a2c0afb36522e3511a32a0307c359e6494c9
shoman <shoma216@gmail.com>
2019-06-11 05:17:00 +0900

    add tests for delete apis

--- todo_server_test.go
+++ todo_server_test.go
@@ -51,7 +51,7 @@ func TestAddTodoSuccess(t *testing.T) {
 	body, _ := ioutil.ReadAll(rsp.Body)
 	var rspMsg Response
 	if err := json.Unmarshal(body, &rspMsg); err != nil {
-		t.Fatal("Response type is not Todo")
+		t.Fatal("Response type is not correct")
 	} else if rspMsg.Status != "success" || rspMsg.Message != "registered" {
 		t.Fatal("Response type or values is not correct")
 	}
@@ -134,6 +134,110 @@ func TestGetTodoFail(t *testing.T) {
 	id := "-1"
 	api := url + "/api/v1/event/" + id
 	req, _ := http.NewRequest("GET", api, nil)
+
+	client := new(http.Client)
+	rsp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("Request failed")
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 404 {
+		t.Fatal("Request failed with status code " + strconv.Itoa(rsp.StatusCode))
+	}
+}
+
+func TestDeleteListSuccess(t *testing.T) {
+	api := url + "/api/v1/event"
+	req, _ := http.NewRequest("DELETE", api, nil)
+
+	client := new(http.Client)
+	rsp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("Request failed")
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 200 {
+		t.Fatal("Request failed with status code " + strconv.Itoa(rsp.StatusCode))
+	}
+
+	body, _ := ioutil.ReadAll(rsp.Body)
+	var rspMsg Response
+	if err := json.Unmarshal(body, &rspMsg); err != nil {
+		t.Fatal("Response type is not correct")
+	} else if rspMsg.Status != "success" || rspMsg.Message != "deleted" {
+		t.Fatal("Response type or values is not correct")
+	}
+}
+
+// For TestDeleteTodoSuccess() & TestDeleteListSuccess()
+// Need two todos at least
+func TestAddTodoSuccess2(t *testing.T) {
+	api := url + "/api/v1/event"
+
+	// Request JSON
+	deadline := "2019-06-11T14:00:00+09:00"
+	title := "レポート提出"
+	memo := "メモ"
+
+	jsonStr := `{"deadline":"` + deadline + `","title":"` + title + `","memo":"` + memo + `"}`
+	req, _ := http.NewRequest(
+		"POST",
+		api,
+		bytes.NewBuffer([]byte(jsonStr)),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := new(http.Client)
+	rsp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("Request failed")
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 200 {
+		t.Fatal("Request failed with status code " + strconv.Itoa(rsp.StatusCode))
+	}
+
+	body, _ := ioutil.ReadAll(rsp.Body)
+	var rspMsg Response
+	if err := json.Unmarshal(body, &rspMsg); err != nil {
+		t.Fatal("Response type is not correct")
+	} else if rspMsg.Status != "success" || rspMsg.Message != "registered" {
+		t.Fatal("Response type or values is not correct")
+	}
+}
+
+func TestDeleteTodoSuccess(t *testing.T) {
+	id := "2"
+	api := url + "/api/v1/event/" + id
+	req, _ := http.NewRequest("DELETE", api, nil)
+
+	client := new(http.Client)
+	rsp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("Request failed")
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode != 200 {
+		t.Fatal("Request failed with status code " + strconv.Itoa(rsp.StatusCode))
+	}
+
+	body, _ := ioutil.ReadAll(rsp.Body)
+	var rspMsg Response
+	if err := json.Unmarshal(body, &rspMsg); err != nil {
+		t.Fatal("Response type is not correct")
+	} else if rspMsg.Status != "success" || rspMsg.Message != "deleted" {
+		t.Fatal("Response type or values is not correct")
+	}
+}
+
+func TestDeleteTodoFail(t *testing.T) {
+	id := "-1"
+	api := url + "/api/v1/event/" + id
+	req, _ := http.NewRequest("DELETE", api, nil)
 
 	client := new(http.Client)
 	rsp, err := client.Do(req)
